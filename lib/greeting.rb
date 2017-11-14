7d16511d161ebb396d1215615735095d0aba7e22
Phoebe Lau <phoebelau7@gmail.com>
2017-11-14 15:45:25 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,4 @@
 # code the #greeting method here!
-puts "What is your name?"
-name = gets.strip
-greeting(name)
-puts "Hello #{name}. It's nice to meet you."
+def greeting (name)
+  puts "Hello #{name}. It's nice to meet you."
+end
